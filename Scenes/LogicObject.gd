50c3ceb7a945dff0c730b06ae70e29d8918fe9f4
cosineblast <55855728+cosineblast@users.noreply.github.com>
2024-04-21 13:49:48 -0300

    Use physics for object picking implementation

--- Scenes/LogicObject.gd
+++ Scenes/LogicObject.gd
@@ -4,8 +4,6 @@ class_name LogicObject
 
 var player_target_position: Marker3D = null
 
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -15,7 +13,30 @@ func _process(delta):
 	pass
 	
 func _integrate_forces(state):
-	pass
-
-func assign_target_position(position: Marker3D):
-	self.player_target_position = position
+	if player_target_position == null:
+		return
+	
+	var this_position = self.global_position
+	var their_position = player_target_position.global_position
+	
+	var difference = their_position - this_position
+	var direction = difference.normalized()
+	
+	var object_velocity = 5
+	
+	if difference.length() > 0.1:
+		state.linear_velocity = direction * 5
+	else:
+		state.linear_velocity *= 0
+		
+func assign_target_position(marker: Marker3D):
+	self.player_target_position = marker
+	self.gravity_scale = 0
+	self.can_sleep = false
+	print(self, ": target position assigned to", marker.global_position)
+	
+func remove_target_position():
+	self.player_target_position = null
+	self.gravity_scale = 1
+	self.can_sleep = true
+	print(self, ": removed target position")
--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -1,6 +1,5 @@
 extends CharacterBody3D
 
-
 const SPEED = 7
 const JUMP_VELOCITY = 8
 
@@ -15,15 +14,12 @@ var ray_cast: RayCast3D = $CameraPivot/RayCast3D
 @onready 
 var ideal_target_position: Marker3D = $CameraPivot/IdealTargetPosition
 
-var target_object: Node3D = null
-
-var target_object_snapshot = null
+var target_object: RigidBody3D = null
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _physics_process(delta):
-	_save_target_object_trasnform()
 	
 	if not is_on_floor():
 		velocity.y -= gravity * delta
@@ -41,10 +37,6 @@ func _physics_process(delta):
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
-	
-	_update_target_object()
-	
-	
 
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
@@ -57,47 +49,32 @@ func _handleMouseMotion(event: InputEventMouseMotion):
 	var multiplier = 1.0/500.0
 		
 	var change = event.relative * multiplier
-
-	_save_target_object_trasnform()
 	
 	camera_pivot.rotate(Vector3(1, 0, 0), -change.y)	
 	rotate(Vector3(0, 1, 0), -change.x)	
 	
-	_update_target_object()
-	
-func _save_target_object_trasnform():
-	if target_object == null:
-		return
-		
-	target_object_snapshot = target_object.global_transform
-		
-func _update_target_object():
-	if target_object == null:
-		return
-		
-	target_object.global_transform = ideal_target_position.global_transform	
-	
-	var would_collide_now = target_object.move_and_collide(Vector3.ZERO, true)
-	
-	if would_collide_now:
-		pass
-		target_object.global_transform = target_object_snapshot
-		
-	
-	
+
 	
 	
 func _handleMouseClick(event: InputEventMouseButton):
 	if event.pressed:
-		
 		var collider = ray_cast.get_collider()
 		
 		if collider == null:
-			return
-		
-		print("Picking object", collider)
+			_drop_item()
 			
-		target_object = collider
-		
-		
+		elif collider.has_method("assign_target_position") and collider != target_object:
+			_pick_item(collider)
 
+
+func _pick_item(target: RigidBody3D):
+	_drop_item()
+	print("Picking object ", target)
+	target.assign_target_position(ideal_target_position)
+	target_object = target
+
+func _drop_item():
+	if target_object != null: 
+		print("Dropping object", target_object)
+		target_object.remove_target_position()
+		target_object = null
