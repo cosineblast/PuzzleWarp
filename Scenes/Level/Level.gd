4aede5d7e62a67d6682ad1a2e45aab4bc2dffafa
cosineblast <55855728+cosineblast@users.noreply.github.com>
2024-05-21 22:06:49 -0300

    Player now drops item after UI is selected

--- Scenes/Level/Level.gd
+++ Scenes/Level/Level.gd
@@ -22,30 +22,31 @@ func _ready():
 
 func show_object_info(logic_object):
 	print("Showing info of ", logic_object)
-	
+
 	gui_container.visible = true
 	crosshair_container.visible = false
-	
+
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-	
+
 	item_ui.current_object = logic_object
-	
+
 	state = "handling_ui"
 	player.ignore_input = true
-	
+
 func hide_object_info():
 	print("Hiding object info")
-	
+
 	gui_container.visible = false
-	crosshair_container.visible = true 
+	crosshair_container.visible = true
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	
+
 	item_ui.current_object = null
-	
+
 	state = "handling_player"
-	
+
+	player.drop_current_item()
 	player.ignore_input = false
-	
+
 func _unhandled_input(event):
 	if state == "handling_player":
 		player.handle_input(event)
--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -11,7 +11,7 @@ var camera_pivot: Marker3D = $CameraPivot
 @onready
 var ray_cast: RayCast3D = $CameraPivot/RayCast3D
 
-@onready 
+@onready
 var ideal_target_position: Marker3D = $CameraPivot/IdealTargetPosition
 
 var ignore_input = false
@@ -24,7 +24,7 @@ func _ready():
 	pass
 
 func _physics_process(delta):
-	
+
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 	elif Input.is_action_just_pressed("ui_accept") and not ignore_input:
@@ -45,36 +45,36 @@ func _physics_process(delta):
 func handle_input(event):
 	if event is InputEventMouseMotion:
 		_handleMouseMotion(event)
-	
+
 	if event is InputEventMouseButton:
 		_handleMouseClick(event)
-		
+
 func _handleMouseMotion(event: InputEventMouseMotion):
 	if ignore_input:
 		return
-		
-	var multiplier = 1.0/500.0
-		
-	var change = event.relative * multiplier
-	
-	camera_pivot.rotate(Vector3(1, 0, 0), -change.y)	
-	rotate(Vector3(0, 1, 0), -change.x)	
-	
 
-	
-	
+	var multiplier = 1.0/500.0
+
+	var change = event.relative * multiplier
+
+	camera_pivot.rotate(Vector3(1, 0, 0), -change.y)
+	rotate(Vector3(0, 1, 0), -change.x)
+
+
+
+
 func _handleMouseClick(event: InputEventMouseButton):
 	if ignore_input:
 		return
-		
+
 	if event.pressed:
-		
+
 		if event.button_index == MouseButton.MOUSE_BUTTON_LEFT:
 			var collider = ray_cast.get_collider()
-		
+
 			if collider == null or collider == target_object:
 				_drop_item()
-			
+
 			elif collider.has_method("assign_target_position"):
 				_pick_item(collider)
 		elif event.button_index == MouseButton.MOUSE_BUTTON_RIGHT:
@@ -89,7 +89,10 @@ func _pick_item(target: RigidBody3D):
 	target_object = target
 
 func _drop_item():
-	if target_object != null: 
+	if target_object != null:
 		print("Dropping object", target_object)
 		target_object.remove_target_position()
 		target_object = null
+
+func drop_current_item():
+	_drop_item()
