d7b77b3bab528c50bf7cc559a1834b7829693387
cosineblast <55855728+cosineblast@users.noreply.github.com>
2024-05-21 21:34:24 -0300

    Start using LogicSpec for Target and LogicLevel

--- Scenes/LogicLevel/LogicLevel.gd
+++ Scenes/LogicLevel/LogicLevel.gd
@@ -1,11 +1,26 @@
 extends Node3D
 
-var inputs: Array = []
+static var LogicSpec = preload("res://Src/LogicSpec.gd")
 
-var outputs: Array = []
+var inputs: Array = []
 
 @onready
 var input_markers: Array = [$Input1, $Input2, $Input3, $Input4, $Input5]
+
+var _output_specs: Array = []
+
+@export
+var outputs: Array[String]:
+	set(value):
+		var result = []
+
+		for string in value:
+			var spec = LogicSpec.parse(string)
+			assert(spec != null)
+			result.append(spec)
+
+		_output_specs = result
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -20,12 +35,7 @@ func _ready():
 		child.position = input_markers[i].position
 		i += 1
 
+	assert(_output_specs.size() == 1)
+	$Target.set_spec(_output_specs[0])
 
-	var outputs_node = $Outputs
-	assert(outputs_node != null)
-
-	for child in outputs_node.get_children():
-		# assert(child.is_class("LogicObject"))
-		outputs.append(child)
-		outputs_node.remove_child(child)
 
--- Scenes/Target/Target.gd
+++ Scenes/Target/Target.gd
@@ -2,9 +2,33 @@ extends Node3D
 
 static var LogicSpec = preload("res://Src/LogicSpec.gd")
 
-@export
-var foo: int = 1
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var _spec
+
+@export
+var spec: String:
+	set(value):
+		set_spec(LogicSpec.parse(value))
+
+
+
+func set_spec(value):
+	assert(value != null)
+
+	_spec = value
+
+	var object = LogicSpec.spawn_object(_spec)
+	var mesh_instance: MeshInstance3D = object.get_node("MeshInstance3D")
+
+	var node = $SubViewport/Node3D
+	var mesh_position = $SubViewport/Node3D/Sample3DMesh
+	
+	mesh_instance.get_parent().remove_child(mesh_instance)
+	node.add_child(mesh_instance)
+	mesh_instance.transform = mesh_position.transform
+
+
+	mesh_instance.layers = 0
+	mesh_instance.set_layer_mask_value(4, true)
+
+
