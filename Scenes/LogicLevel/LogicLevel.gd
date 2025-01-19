4e999133e15fc7d42c71df3ec8c30ebc380ba424
cosineblast <55855728+cosineblast@users.noreply.github.com>
2025-01-19 14:47:21 -0300

    Logic level now loads targets from input attribute

--- Scenes/LogicLevel/LogicLevel.gd
+++ Scenes/LogicLevel/LogicLevel.gd
@@ -1,11 +1,16 @@
 extends Node3D
 
 const LogicSpec = preload("res://Src/LogicSpec.gd")
+const LevelTargetScene = preload("res://Scenes/LevelTarget/LevelTarget.tscn")
 
 var inputs: Array = []
 
+
 @onready
-var input_markers: Array = [$Input1, $Input2, $Input3, $Input4, $Input5]
+var _input_markers: Array = [$Input1, $Input2, $Input3, $Input4, $Input5]
+
+@onready
+var _output_markers: Array = [$Output1, $Output2, $Output3, $Output4, $Output5]
 
 var _output_specs: Array = []
 
@@ -22,8 +27,35 @@ var outputs: Array[String]:
 		_output_specs = result
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
+
+
+	_load_targets()
+	_load_inputs()
+
+
+func _load_targets():
+	var i = 0
+
+	# right now the output count is limited to the number of output markers
+	assert(len(_output_specs) < len(_output_markers))
+
+	while i < len(_output_specs):
+		var spec = _output_specs[i]
+		var target = LevelTargetScene.instantiate()
+		target.spec = spec
+
+		target.matching_object_touched.connect(func(_thing): 
+			target.closed = true)
+
+		add_child(target)
+		target.position = _output_markers[i].position
+		i += 1
+
+
+
+
+func _load_inputs():
 	# When the level loads, we look at the 'Inputs' node 
 	# (which shall be added by the instantiator of this node)
 	# to find the logic objects this level receives as input
@@ -39,7 +71,7 @@ func _ready():
 
 	for child in inputs_node.get_children():
 		inputs.append(child)
-		child.position = input_markers[i].position
+		child.position = _input_markers[i].position
 		i += 1
 
 	# TODO: Add logic specs to targets
